46dbcb27c646ee5e01af78bc0b4d1a3ef28fef63
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-01-16 09:31:31 +0200

    gstock updated

---
+++ dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Tableau de Bord</title>
+</head>
+<body>
+    <header>
+        <div class="admin">
+            <div class="img">
+                <img src="images/2.jpg" alt="Image non disponible" height="95" width="95" class="img-admin">
+            </div>
+            <div class="txt">
+                <h3>Admin</h3>
+                <p>voir profil</p>
+            </div>
+            </div>
+            <div class="log-out">
+                <a href="index.php">deconnexion</a>
+            </div>
+        
+    </header>
+    <div id="boody">
+    <nav>
+        <div class="menu-link">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Entrée</a></li>
+                <li><a href="#">Sortie</a></li>
+                <li><a href="#">Stock</a></li>
+                <li><a href="#">Fournisseur</a></li>
+                <li><a href="#">Categorie</a></li>
+                <li><a href="#">Journal</a></li>
+                <li><a href="#">Utilisateur</a></li>
+                <li><a href="#">Statistique</a></li>
+        </div>
+    </nav>
+    <section>
+        
+    </section>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentification</title>
+    <style>
+        body{
+            background-image: url("../Gstock/images/1.jpg");
+            background-repeat: no-repeat;
+            background-size:cover;
+        }
+    fieldset{
+        width:400px;
+        margin-left:400px;
+        margin-top:200px;
+        background-color: black;
+        border-radius: 20px;
+        opacity: 0.7;
+    }
+    #email{
+        height:30px;
+        width:250px;
+    }
+    #password{
+        height:30px;
+        width:250px;
+     
+    }
+    legend{
+        font-family:Arial, Helvetica, sans-serif;
+        font-size: 20px;
+        color:ivory;
+    }
+    th{
+        font-family: Arial, Helvetica, sans-serif;
+        color:ivory;
+    }
+    #sub{
+        height:30px;
+    }
+    #sub:hover{
+        color:ivory;
+        background: gray;
+        transition:0.7s;
+    }
+    </style>
+</head>
+<body>
+    <fieldset>
+        <legend>Connectez-vous</legend>
+    <form method="post">
+        <table>
+            <tr>
+                <th>EMAIL:</th>
+                <td><input type="email" name="email" id="email" placeholder="votre email..." required></td>
+            </tr>
+            <tr>
+                <th>MOT DE PASSE:</th>
+                <td><input type="password" name="motdepasse" id="password" placeholder="**********" required></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="Connexion" id="sub"></td>
+            </tr>
+        </table>   
+    </form>
+    </fieldset>
+</body>
+</html>
