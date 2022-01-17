5b35e6ce479609047a932e4e0e637cff23c34014
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-01-17 22:31:29 +0200

    journal,statistique,utilisateur added and style updated

---
+++ journal.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Journal</title>
+</head>
+<body>
+    <header>
+        <div class="admin">
+            <div class="img">
+                <img src="images/2.jpg" alt="Image non disponible" height="75" width="75" class="img-admin">
+            </div>
+            <div class="txt">
+                <h3>Admin</h3>
+                <p><a href="#" class="voir">voir profil</a></p>
+            </div>
+            </div>
+            <div class="log-out">
+                <a href="index.php" class="decon">Log-out   <i class="fas fa-power-off"></i></a>
+            </div>
+        
+    </header>
+    <div id="boody">
+    <nav>
+        <div class="menu-link">
+            <ul>
+                <li><a href="#">Dashboard    <i class="fas fa-tachometer-alt"></i></a></li>
+                <li><a href="#">Entrée    <i class="fas fa-cart-plus"></i></a></li>
+                <li><a href="#">Sortie    <i class="fas fa-shopping-bag"></i></a></li>
+                <li><a href="#">Stock   <i class="fas fa-warehouse"></i></a></li>
+                <li><a href="#">Fournisseur     <i class="fas fa-user-plus"></i></a></li>
+                <li><a href="#">Categorie     <i class="fas fa-sort"></i></a></li>
+                <li><a href="#">Journal     <i class="fas fa-newspaper"></i></a></li>
+                <li><a href="#">Utilisateur     <i class="fas fa-users"></i></a></li>
+                <li><a href="#">Statistique    <i class="fas fa-chart-line"></i></a></li>
+        </div>
+    </nav>
+    <section>
+        <div class="journ">
+            <h2 class="title-journ1">Journal d'Entrée</h2>
+        </div>
+        <div class="journ1">
+            <h2 class="title-journ2">Journal de Sortie</h2>
+        </div>
+    </section>
+    </div>
+</body>
+</html>
---
+++ utilisateur.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Utilisateur</title>
+</head>
+<body>
+    <header>
+        <div class="admin">
+            <div class="img">
+                <img src="images/2.jpg" alt="Image non disponible" height="75" width="75" class="img-admin">
+            </div>
+            <div class="txt">
+                <h3>Admin</h3>
+                <p><a href="#" class="voir">voir profil</a></p>
+            </div>
+            </div>
+            <div class="log-out">
+                <a href="index.php" class="decon">Log-out   <i class="fas fa-power-off"></i></a>
+            </div>
+        
+    </header>
+    <div id="boody">
+    <nav>
+        <div class="menu-link">
+            <ul>
+                <li><a href="#">Dashboard    <i class="fas fa-tachometer-alt"></i></a></li>
+                <li><a href="#">Entrée    <i class="fas fa-cart-plus"></i></a></li>
+                <li><a href="#">Sortie    <i class="fas fa-shopping-bag"></i></a></li>
+                <li><a href="#">Stock   <i class="fas fa-warehouse"></i></a></li>
+                <li><a href="#">Fournisseur     <i class="fas fa-user-plus"></i></a></li>
+                <li><a href="#">Categorie     <i class="fas fa-sort"></i></a></li>
+                <li><a href="#">Journal     <i class="fas fa-newspaper"></i></a></li>
+                <li><a href="#">Utilisateur     <i class="fas fa-users"></i></a></li>
+                <li><a href="#">Statistique    <i class="fas fa-chart-line"></i></a></li>
+        </div>
+    </nav>
+    <section>
+        <h2 class="title">Utilisateur</h2>
+        <div class="users">
+            <h3 class="stit-user">Ajouter un Utilisateur</h3>
+            <fieldset class="fieldset9">
+                <table>
+                    <tr>
+                     <th class="th-user">Email</th>
+                        <th><input type="text" name="" class="inpt-user"placeholder="Email..."></th>
+                    </tr>
+                    <tr>
+                     <th class="th-user">Mot de Passe</th>
+                        <td><input type="password" name="" class="inpt-user" placeholder="Mot de Passe..."></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Ajouter" id="sub-user"></td>
+                   <td> <input type="reset" value="Annuler" id="res-user"></td>
+                    </tr>
+                </table>
+            </fieldset>
+        </div>
+        <div class="recup-user">
+        <h3 class="s-tituser">Liste des Utilisateurs</h3>
+            <fieldset class="fieldset01">
+            <table>
+                    <thead class="thead-user">
+                       <tr>
+                           <th class="th2-user">Email</th>
+                           <th class="th2-user">Mot de Passe</th>
+                           <th class="th2-user"colspan="2">Actions</th>
+                       </tr>
+                   </thead>
+                   <tbody class="tbody-user">
+                       <tr>
+                           <td class="td-user"></td>
+                           <td class="td-user"></td>
+                           <td class="td-user"></td>
+                           <td class="td-user"></td>
+                       </tr>
+                   </tbody>
+                    </table>
+            </fieldset>
+        </div>
+    </section>
+    </div>
+</body>
+</html>
