a270d3ecfb728f2840cc0530d017ce5bfa33c945
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-01-19 22:44:55 +0200

    connecting database in all pages

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,7 @@
+<?php
+include("database.php");
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
---
+++ database.php
@@ -0,0 +1,3 @@
+<?php
+$bdd=new PDO('mysql:host=localhost;dbname=gstock;charset=utf8','root','');
+?>
