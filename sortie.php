e03fb51062c7ba4da963e3b96b7e3a7b13fd3ca2
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-01-25 23:42:14 +0200

    favicon added

--- sortie.php
+++ sortie.php
@@ -7,6 +7,7 @@ include("database.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/03.png" type="image/x-icon">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>Sortie Stock</title>
