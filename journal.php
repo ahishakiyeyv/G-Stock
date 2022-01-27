c8eb731801d3a8c5bb3688a5c36d4f4f70cccfe9
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-01-27 23:02:31 +0200

    header & menu modified

--- journal.php
+++ journal.php
@@ -13,14 +13,14 @@ include("database.php");
     <title>Journal</title>
 </head>
 <body>
-    <header>
+<header>
         <div class="admin">
             <div class="img">
                 <img src="images/2.jpg" alt="Image non disponible" height="75" width="75" class="img-admin">
             </div>
             <div class="txt">
-                <h3>Admin</h3>
-                <p><a href="profil.php" class="voir">voir profil</a></p>
+                <h3 class="adm">Admin</h3>
+                <p class="prof"><a href="profil.php" class="voir">voir profil</a></p>
             </div>
             </div>
             <div class="log-out">
@@ -31,17 +31,17 @@ include("database.php");
     <div id="boody">
     <nav>
         <div class="menu-link">
-            <ul>
-            <li><a href="dashboard.php"><i class="fas fa-tachometer-alt"></i>  --  Dashboard</a></li>
-                <li><a href="entree.php"><i class="fas fa-cart-plus"></i>   --   Entrée</a></li>
-                <li><a href="sortie.php"><i class="fas fa-shopping-bag"></i>  --  Sortie</a></li>
-                <li><a href="produit.php"><i class="fas fa-dolly-flatbed"></i>   --   Produit </a></li>
-                <li><a href="stock.php"><i class="fas fa-warehouse"></i>   --     Stock </a></li>
-                <li><a href="fournisseur.php"><i class="fas fa-user-plus"></i>   --   Fournisseur</a></li>
-                <li><a href="categorie.php"><i class="fas fa-sort"></i>      --   Categorie</a></li>
-                <li><a href="journal.php"><i class="fas fa-newspaper"></i>   --   Journal</a></li>
-                <li><a href="utilisateur.php"><i class="fas fa-users"></i>   --  Utilisateur</a></li>
-                <li><a href="statistique.php"><i class="fas fa-chart-line"></i>     --    Statistique</a></li>
+            <ul class="ul">
+            <li class="li"><a class="link" href="dashboard.php"><i class="fas fa-tachometer-alt"></i>  --  Dashboard</a></li>
+                <li class="li"><a class="link" href="entree.php"><i class="fas fa-cart-plus"></i>   --   Entrée</a></li>
+                <li class="li"><a class="link" href="sortie.php"><i class="fas fa-shopping-bag"></i>  --  Sortie</a></li>
+                <li class="li"><a class="link" href="produit.php"><i class="fas fa-dolly-flatbed"></i>   --   Produit </a></li>
+                <li class="li"><a class="link" href="stock.php"><i class="fas fa-warehouse"></i>   --     Stock </a></li>
+                <li class="li"><a class="link" href="fournisseur.php"><i class="fas fa-user-plus"></i>   --   Fournisseur</a></li>
+                <li class="li"><a class="link" href="categorie.php"><i class="fas fa-sort"></i>      --   Categorie</a></li>
+                <li class="li"><a class="link" href="journal.php"><i class="fas fa-newspaper"></i>   --   Journal</a></li>
+                <li class="li"><a class="link" href="utilisateur.php"><i class="fas fa-users"></i>   --  Utilisateur</a></li>
+                <li class="li"><a class="link" href="statistique.php"><i class="fas fa-chart-line"></i>     --    Statistique</a></li>
         </div>
     </nav>
     <section id="section">
