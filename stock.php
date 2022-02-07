850ed1a73a9871088c8401dcb5e3e95eeccc9f46
ahishakiyeyv <yvanahishakiye6@gmail.com>
2022-02-07 22:16:52 +0200

    style modified

--- stock.php
+++ stock.php
@@ -9,7 +9,7 @@ include("database.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/03.png" type="image/x-icon">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Stock</title>
 </head>
 <body>
